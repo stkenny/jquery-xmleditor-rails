c27132977c93c497aeb917168693420f399f3f2c
Stuart Kenny <skenny@tchpc.tcd.ie>
2016-08-12 21:19:48 +0100

    Import cycle js from assets

--- lib/jquery/xmleditor/rails/engine.rb
+++ lib/jquery/xmleditor/rails/engine.rb
@@ -2,11 +2,6 @@ module JQuery
   module XmlEditor
     module Rails
       class Engine < ::Rails::Engine
- 
-        # Initializer to combine this engines static assets with the static assets of the hosting site.
-        initializer "static assets" do |app|
-          app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-        end
 
       end
     end
